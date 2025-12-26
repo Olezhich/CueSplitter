0bc2609644b913ed483a7bf68c1263c4131a1472
Olezhich <qwertypx@yandex.ru>
2025-12-26 12:44:18 +0300

    added pretty errors output

--- cuesplitter/cli.py
+++ cuesplitter/cli.py
@@ -1,20 +1,40 @@
 from pathlib import Path
 from cuesplitter.core import split_album, parse_album
 
+from cuetools import CueParseError, CueValidationError
+
 import typer
 
+from rich.console import Console
 
+
+app = typer.Typer()
+
+stdout = Console()
+stderr = Console(stderr=True)
+
+@app.command()
 def split(input: Path, output: Path = Path(), dry: bool = False):
     """
     Split album on different tracks by `.cue` file
     """
-    if dry:
-        album = parse_album(input)
-        for track in album.tracks:
-            print(track)
-    else:
-        split_album(input, output)
+    try:
+        if dry:
+            album = parse_album(input)
+            for track in album.tracks:
+                stdout.print(track)
+        else:
+            split_album(input, output)
+    except (CueValidationError) as e:
+        stderr.print('[bold red]Cue validation error:[/bold red]')
+        stderr.print(str(e))
+        raise typer.Exit(code=1)
+    except (CueParseError) as e:
+        stderr.print('[bold red]Cue parse error:[/bold red]')
+        stderr.print(str(e))
+        raise typer.Exit(code=1)
+
 
 
 if __name__ == '__main__':
-    typer.run(split)
+    app()
